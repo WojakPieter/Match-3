3d5f74d768ed177adad54c1d2bb57b224fc5fc8e
sspaczyn <01168915@pw.edu.pl>
2022-01-23 22:23:06 +0100

    Added docstrings

--- save_and_read_to_yml.py
+++ save_and_read_to_yml.py
@@ -5,12 +5,21 @@ name_of_result_file = 'result.yml'
 
 
 def save_result_to_file(name, points):
+    '''
+        Opens result yaml file and saves name of player and his points. Terminates a program afterwards.
+    '''
     file = open(name_of_result_file, 'a')
     yaml.dump({name: points}, file)
     exit(0)
 
 
 def find_max_result():
+    '''
+        Opens result yaml file and reads the best result.
+
+        Returns:
+            int: the maximum value from file, if the file does not exist, returns 0
+    '''
     try:
         file = open(name_of_result_file, 'r')
         max_result = 0
