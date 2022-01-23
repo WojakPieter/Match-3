fd23889e1318700616e249b900e2e6ca0b06da8a
sspaczyn <01168915@pw.edu.pl>
2022-01-23 22:27:22 +0100

    Test message

--- sequences.py
+++ sequences.py
@@ -1,8 +1,30 @@
+class NotQuadraticListError(Exception):
+    def __init__(self):
+        super().__init__("Nested sublists don't have the same length that original list")
+
+
 class Sequence:
+    '''
+        Class responsible for finding a set of at least three same elements in a row or column
+        in two-dimensional list.
+
+        Args:
+            list_of_elements (list): two-dimensional list (it must be quadratic)
+
+    '''
     def __init__(self, list_of_elements):
+        for sublist in list_of_elements:
+            if len(sublist) != len(list_of_elements):
+                raise NotQuadraticListError
         self.list_of_elements = list_of_elements
 
     def findSequences(self):
+        '''
+            Finds coordinates of three or more same elements in row or column in a list  (counted from 0).
+
+            Returns:
+                list: list of tuples with coordinates
+        '''
         sequence_squares = []
         # vertical
         for x in range(len(self.list_of_elements)):
@@ -110,6 +132,13 @@ class Sequence:
                 return [(x + 1, y), (x + 1, y + 1)]
 
     def findPossibleSequences(self):
+        '''
+            Finds coordinates of every two elements in a table, which swapped will create
+            a sequence of at least three repeating elements in a line.
+
+            Returns:
+                list: list of sublists, each with two tuples
+        '''
         possible_sequences = []
         list = self.list_of_elements
         for x in range(len(list)):
