0c414d94285f2e93d40895a3994950e3d52552d6
sspaczyn <01168915@pw.edu.pl>
2022-01-24 23:04:37 +0100

    Created finctions to open file

--- save_and_read_to_yml.py
+++ save_and_read_to_yml.py
@@ -4,12 +4,21 @@ import yaml
 name_of_result_file = 'result.yml'
 
 
+def open_file_to_read(name):
+    return open(name, 'r')
+
+
+def open_file_to_save(name):
+    return open(name, 'a')
+
+
 def save_result_to_file(name, points):
     '''
         Opens result yaml file and saves name of player and his points. Terminates a program afterwards.
     '''
-    file = open(name_of_result_file, 'a')
+    file = open_file_to_save(name_of_result_file)
     yaml.dump({name: points}, file)
+    file.close()
     exit(0)
 
 
@@ -21,7 +30,7 @@ def find_max_result():
             int: the maximum value from file, if the file does not exist, returns 0
     '''
     try:
-        file = open(name_of_result_file, 'r')
+        file = open_file_to_read(name_of_result_file)
         max_result = 0
         for line in file:
             if line.strip() != '':
