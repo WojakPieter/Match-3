0345a4db1efe318bd6162d5749d8846fd37f3cfc
sspaczyn <01168915@pw.edu.pl>
2022-01-24 23:02:17 +0100

    Added exception when there is no config file

--- main.py
+++ main.py
@@ -8,19 +8,23 @@ from board import Board
 
 
 def Main():
-    file = open(name_of_config_file, 'r')
-    board_size = read_board_size_from_json(file)
-    file.close()
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    board = Board(ui, board_size)
-    ui.button_swap_buttons.clicked.connect(board.swap_sequences)
-    file = open(name_of_config_file, 'r')
-    file.close()
-    MainWindow.show()
-    sys.exit(app.exec_())
+    try:
+        file = open(name_of_config_file, 'r')
+        board_size = read_board_size_from_json(file)
+        file.close()
+        app = QtWidgets.QApplication(sys.argv)
+        MainWindow = QtWidgets.QMainWindow()
+        ui = Ui_MainWindow()
+        ui.setupUi(MainWindow)
+        board = Board(ui, board_size)
+        board.show()
+        ui.button_swap_buttons.clicked.connect(board.swap_sequences)
+        file = open(name_of_config_file, 'r')
+        file.close()
+        MainWindow.show()
+        sys.exit(app.exec_())
+    except FileNotFoundError:
+        print('No config file in directory!')
 
 
 if __name__ == "__main__":
